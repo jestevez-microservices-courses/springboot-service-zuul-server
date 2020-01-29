c3ea458d606c65ff36b2b0fdf6bf4a476e0d85d3
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-29 11:57:01 +0100

    New Spring Boot project - Zuul Server

---
+++ src/test/java/com/joseluisestevez/msa/zuul/SpringbootServiceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
