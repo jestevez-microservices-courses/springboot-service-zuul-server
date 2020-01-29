3e4a6559bca63add4332353727cd4f3d81c7fc51
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-29 12:01:04 +0100

    Enable Zuul Server and Eureka Client

--- src/main/java/com/joseluisestevez/msa/zuul/SpringbootServiceZuulServerApplication.java
+++ src/main/java/com/joseluisestevez/msa/zuul/SpringbootServiceZuulServerApplication.java
@@ -2,12 +2,17 @@ package com.joseluisestevez.msa.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class SpringbootServiceZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServiceZuulServerApplication.class, args);
+		SpringApplication.run(SpringbootServiceZuulServerApplication.class,
+				args);
 	}
 
 }
