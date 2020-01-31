1376a6b7e6fe426f15949a2112d323839baa832c
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-31 10:56:07 +0100

    Enable configuration client and move secret to config-server

--- src/main/java/com/joseluisestevez/msa/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/joseluisestevez/msa/zuul/oauth/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.joseluisestevez.msa.zuul.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,9 +12,13 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources)
@@ -52,7 +58,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("HARD_CODE_SECRET_CODE");
+		tokenConverter.setSigningKey(jwtKey);
 		return tokenConverter;
 	}
 
