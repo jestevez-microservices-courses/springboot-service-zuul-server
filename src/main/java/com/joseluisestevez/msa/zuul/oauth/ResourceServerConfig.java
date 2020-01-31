a085dc2354db9408f944923dbaba37e6b57ab5f5
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-31 12:05:25 +0100

    Allow invocations from other domains with CORS (Cross Origin Resource Sharing)

--- src/main/java/com/joseluisestevez/msa/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/joseluisestevez/msa/zuul/oauth/ResourceServerConfig.java
@@ -1,9 +1,13 @@
 package com.joseluisestevez.msa.zuul.oauth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +15,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
@@ -52,7 +60,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 						"/api/users/**")
 				.hasRole("ADMIN")
 				// Al final cualquier otra ruta necesita autenticacion
-				.anyRequest().authenticated();
+				.anyRequest().authenticated().and().cors()
+				.configurationSource(corsConfigurationSource());
 	}
 
 	@Bean
@@ -67,4 +76,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		return new JwtTokenStore(accessTokenConverter());
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		// corsConfig.addAllowedOrigin("http://localhost:4200"); // angular
+		// corsConfig.addAllowedOrigin("*"); // todos
+		corsConfig
+				.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+		corsConfig.setAllowedMethods(
+				Arrays.asList("POST", "PUT", "GET", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(
+				Arrays.asList("Autorization", "Content-Type"));
+
+		// Rutas que aplica la configuracion
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		// Prioridad alta
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
